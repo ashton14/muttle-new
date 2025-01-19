7f826c9c03e2de987a1bb8c594e155afb6cc6c51
ashton14 <ashtonalonge14@gmail.com>
2025-01-18 19:14:44 -0800

    port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,6 @@ import 'reflect-metadata';
 import app from './server/app';
 
 const port = app.get('port');
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`App Listening on port ${port}`);
 });
